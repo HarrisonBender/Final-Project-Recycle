7e6a07ae5e1ec50f30862b4ce473df9338093195
im2dvine <daesydesign@gmail.com>
2021-08-08 02:27:13 -0500

    updated notes

--- src/server/routes/routes.ts
+++ src/server/routes/routes.ts
@@ -2,7 +2,6 @@ import * as express from "express";
 import DB from "../db";
 import commentsRouter from "./comments";
 import recycleCentersRouter from "./recycleCenters";
-// import usersRouter from './users';
 
 const router = express.Router();
 
@@ -10,6 +9,5 @@ router.use("/comments", commentsRouter);
 
 router.use("/recycleCenters", recycleCentersRouter);
 
-// router.use("/users", usersRouter);
 
 export default router;
