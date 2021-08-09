20e68539c1cbf853b56ae2fbac56fa3ff53f0c1b
HarrisonBender <Harrison@MacBook-Pro.local>
2021-08-09 17:08:06 -0500

    Updated centerMaterials Query

--- src/server/db/centerMaterials.ts
+++ src/server/db/centerMaterials.ts
@@ -7,7 +7,16 @@ const one = (id: string) => Query(`
 SELECT * FROM centerMaterials WHERE materials.id = ?;
 `,[id]);
 
+const getCenter = ( id: string) => Query (`
+ select recycleCenters.name, recycleCenters.addr, recycleCenters.hours, recycleCenters.countyid 
+from recycleCenters
+join centermaterials on recycleCenters.id = centerid
+where centerMaterials.materialsid = ?
+`, [id])
+
+
 export default {
   all,
   one,
+  getCenter
 };
